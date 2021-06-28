d452db42d9e354b51beb8e74a69b462c55d238f8
Danny <dannylockhart91@gmail.com>
2021-06-28 11:13:53 +0100

    - Added basic get/post routes - Start with in-mem data-base - Implemented basic DTO and object relation

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.DTOs;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IInMemItemsRepo repository;
+
+        public ItemsController(IInMemItemsRepo repository)
+        {
+            this.repository = repository;
+        }
+
+        // Get /items
+        [HttpGet]
+        public IEnumerable<ItemDTO> GetItems()
+        {
+            var items = repository.GetItems().Select(item => item.AsDTO());
+            return items;
+        }
+
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<ItemDTO> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return Ok(item.AsDTO());
+        }
+
+        // POST /items
+        [HttpPost]
+        public ActionResult<ItemDTO> CreateItem(CreateItemDTO itemDTO)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDTO.Name,
+                Price = itemDTO.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { Id = item.Id }, item.AsDTO());
+        }
+    }
+}
---
+++ DTOs/CreateItemDTO.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.DTOs
+{
+    public record CreateItemDTO
+    {
+        [Required]
+        public string Name { get; init; }
+
+        [Required]
+        [Range(1, 1000)]
+        public decimal Price { get; init; }
+    }
+}
